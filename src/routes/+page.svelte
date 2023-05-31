617a8112135dbdba0156b1c85e99b8e52a3d9f67
Kyler Chin <kyler@kylers-surface>
2023-05-30 21:40:09 -0700

    first front page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<div class="w-full">
+	<nav
+		class="w-full bg-zinc-900 border-b border-gray-100 align-center px-4 py-2 flex flex-row gap-x-2"
+	>
+		<img class="w-12" src="/logo.png" />
+		<div class="flex flex-col">
+			<h1 class="text-gray-50 text-lg">Kyler Chin</h1>
+			<p class="text-gray-100">Computer Scientist & Programmer</p>
+		</div>
+		<div class="flex flex-row gap-x-4 my-2 ml-auto">
+			<a href="https://github.com/kylerchin"><p class="text-gray-100 underline">GitHub</p></a>
+			<a href="mailto:kylerschin@gmail.com"><p class="text-gray-100 underline">Contact</p></a>
+		</div>
+	</nav>
+	<div class="bg-zinc-800 text-gray-100">
+		<div class="px-2 py-8 md:py-24 lg:py-30 px-12 md:px-30 lg:px-40 flex flex-col md:flex-row">
+			<div class="flex flex-col justify-center align-center">
+				<h1 class="text-xl md:text-2xl lg:text-3xl">Hi, I'm Kyler!</h1>
+				<p class="max-w-sm">
+					I research public transit routing & computing, computational environmental science,
+					open-source systems, and computing for equity/public good.
+				</p>
+			</div>
+			<div class="md:ml-auto max-w-sm hidden lg:block">
+				<img src="/IMG_20230529_223233_634.jpg" class=" lg:max-w-auto" />
+				<p class="pt-1 text-sm italic">
+					Kyler mentoring / debugging participants code at VenusHacks UC Irvine hackathon 23.05.26
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<!-- Add a grid of components -->
+	</div>
+</div>
